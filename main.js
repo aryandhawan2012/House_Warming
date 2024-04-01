33adc4430902ece1fc26b9f1271ee35579defcd9
aryandhawan2012 <83532570+aryandhawan2012@users.noreply.github.com>
2024-04-01 15:57:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload() {
+    window.location = "index.html";
+}
+
+function onetotwo() {
+    window.location = "Aryan.html";
+}   
+
+function twotoone() {
+    window.location = "index.html";
+}
+
+function twotothree() {
+    window.location = "address.html"
+}
+
+function threetotwo() {
+    window.location = "Aryan.html"
+}
